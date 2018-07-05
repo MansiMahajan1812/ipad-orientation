a764b299ecde1ae404056ebf7b0993360dc5cbd8
Mansi Mahajan <Mansi@amrits-mac-mini.local>
2018-07-05 18:53:05 +0530

    Initial Commit

---
+++ ipadOrientation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ipadOrientation
+//
+//  Created by Mansi Mahajan on 7/5/18.
+//  Copyright © 2018 Mansi Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
